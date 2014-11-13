e77a35a4762b0ffc2e3a81b1e48fd7f76df818d5
jonahsmith <smith.jonah@gmail.com>
2014-11-13 12:46:45 -0500

    Update for morning

--- analysis.R
+++ analysis.R
@@ -12,4 +12,17 @@ y15$Council.Member <- gsub("TreygerÊ", "Treyger", y15$Council.Member)
 ## cleanup
 y15$Council.Member <- as.factor(y15$Council.Member)
 
-# 
+# Cleanup Legal.Name.of.Organization
+length(levels(y15$Legal.Name.of.Organization)) #start with __
+
+y15$Legal.Name.of.Organization <- gsub("^\\s+|\\s+$", "", y15$Legal.Name.of.Organization)
+length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+
+y15$Legal.Name.of.Organization <- gsub("^\\s+|\\s+$", "", y15$Legal.Name.of.Organization) #Inc. <- Inc
+length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+
+#  ", Inc" <- [A-Za-z] \\s Inc (except not quite because this would replace the last character)
+length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+
+# Uppercase everything
+length(levels(y15$Legal.Name.of.Organization)) #reduce to __
