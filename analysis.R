6f2ce30750d28c0d895624df049c41b6a9f451d4
jonahsmith <smith.jonah@gmail.com>
2014-11-14 16:56:34 -0500

    mostly cleaning organization names

--- analysis.R
+++ analysis.R
@@ -1,28 +1,52 @@
 setwd("~/Dropbox/school/politics/story/discretionary/data/")
 
-y15<- read.csv("funded_disclosure_FY2015.csv", fileEncoding= "latin1")
+y14 <- read.csv("funded_disclosure_FY2014.csv", fileEncoding= "latin1")
+y15 <- read.csv("funded_disclosure_FY2015.csv", fileEncoding= "latin1")
 
-# Cleanup Councilmember names
+y14$Year <- 2014
+y15$Year <- 2015
+
+y14$Status <- NA
+y15$X. <- NA
+
+funding <- rbind(y14,y15)
+
+# Clean up Councilmember names
 ## trailing whitespace
-y15$Council.Member <- gsub("\\s+$", "", y15$Council.Member)
+funding$Council.Member <- gsub("^\\s+|\\s+$", "", funding$Council.Member)
 
-## fix Treyger (what is that symbol?)
-y15$Council.Member <- gsub("TreygerÊ", "Treyger", y15$Council.Member)
+## uppercase
+funding$Council.Member <- toupper(funding$Council.Member)
+
+## fix Treyger and Gonzalez (non-unicode characters)
+funding$Council.Member <- gsub("TREYGERÊ", "TREYGER", funding$Council.Member)
+funding$Council.Member <- gsub("\u0087", "A", funding$Council.Member)
 
 ## cleanup
-y15$Council.Member <- as.factor(y15$Council.Member)
+funding$Council.Member <- as.factor(funding$Council.Member)
 
 # Cleanup Legal.Name.of.Organization
-length(levels(y15$Legal.Name.of.Organization)) #start with __
+length(levels(as.factor(funding$Legal.Name.of.Organization))) #3854
 
-y15$Legal.Name.of.Organization <- gsub("^\\s+|\\s+$", "", y15$Legal.Name.of.Organization)
-length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+## leading and trailing spaces
+funding$Legal.Name.of.Organization <- gsub("^\\s+|\\s+$", "", funding$Legal.Name.of.Organization)
+length(levels(as.factor(funding$Legal.Name.of.Organization))) #3730
 
-y15$Legal.Name.of.Organization <- gsub("^\\s+|\\s+$", "", y15$Legal.Name.of.Organization) #Inc. <- Inc
-length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+## upper case
+funding$Legal.Name.of.Organization <- toupper(funding$Legal.Name.of.Organization)
+length(levels(as.factor(funding$Legal.Name.of.Organization))) #3702
 
-#  ", Inc" <- [A-Za-z] \\s Inc (except not quite because this would replace the last character)
-length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+## remove commas, periods, and apostrophes
+funding$Legal.Name.of.Organization <- gsub("[.,']", "", funding$Legal.Name.of.Organization)
+length(levels(as.factor(funding$Legal.Name.of.Organization))) #3604
 
-# Uppercase everything
-length(levels(y15$Legal.Name.of.Organization)) #reduce to __
+## remove inc, corp (as its own word to avoid corps), and corporation
+funding$Legal.Name.of.Organization <- gsub("\\s+INC|\\s+CORP|\\s+CORPORATION", "", funding$Legal.Name.of.Organization)
+length(levels(as.factor(funding$Legal.Name.of.Organization))) #3465
+
+## change "and"
+funding$Legal.Name.of.Organization <- gsub("&", "AND", funding$Legal.Name.of.Organization)
+length(levels(as.factor(funding$Legal.Name.of.Organization))) #3460
+
+## cleanup
+funding$Legal.Name.of.Organization <- as.factor(funding$Legal.Name.of.Organization)
